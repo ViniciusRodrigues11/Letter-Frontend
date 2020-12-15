ce918bc8670b9962e91b912c6a8db7cfc4a41739
Vinicius Rodrigues da Silva <vinicius.silva@saolucas.edu.br>
2020-12-15 18:16:37 -0400

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import Routes from './routes'
 
-
-import GlobalStyle from './style/global'
-
-import Snowfall from 'react-snowfall'
 function App() {
   return (
     <>
       <Routes />
-      <GlobalStyle />
-      <Snowfall
-        snowflakeCount={10}
-      />
+     
     </>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,22 @@ import CreateLetter from '../Pages/CreateLetter'
 import Landing from '../Pages/Landing/index'
 import Letter from '../Pages/Letter/index'
 
+
+import GlobalStyle from '../style/global'
+import Snowfall from 'react-snowfall'
+
 const Routes: React.FC = () => (
-        <BrowserRouter>
+    <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Landing} />
             <Route path="/create" component={CreateLetter} />
             <Route path="/letter/:id" component={Letter} />
         </Switch>
-        </BrowserRouter>
+        <GlobalStyle />
+        <Snowfall
+            snowflakeCount={10}
+        />
+    </BrowserRouter>
 
 )
 
